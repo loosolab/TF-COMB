Fix for #23 due to floating point errors in background variance/mean estimation

tfcomb/objects.py
@@ -1828,7 +1828,8 @@ class DiffCombObj():
 						   measure="cosine", 
 						   measure_threshold=None,
 						   pvalue_threshold=0.05,
-						   plot = True, 
+						   plot = True,
+						   pseudocount = 10**-10, 
 						   **kwargs):
 		"""
 		Select differentially regulated rules on the basis of measure and pvalue.
@@ -1845,6 +1846,8 @@ class DiffCombObj():
 			The p-value threshold for selecting rules. Default: 0.05.
 		plot : boolean, optional
 			Whether to plot the volcano plot. Default: True.
+		pseudocount : float
+			Pseudocount to add to p-values before transforming. Default: 10^-10.
 
 		Returns
 		----------
@@ -1883,7 +1886,8 @@ class DiffCombObj():
 		if plot == True:
 			cp = self.rules.copy() #ensures that -log10 col is not added to self.rules
 			log_col = "-log10({0})".format(pvalue_col)
-			cp[log_col] = -np.log10(self.rules[pvalue_col])
+
+			cp[log_col] = -np.log10(self.rules[pvalue_col] + pseudocount)
 			log_threshold = -np.log10(pvalue_threshold)
 			
 			tfcomb.plotting.scatter(cp, 

tfcomb/utils.py
@@ -58,7 +58,6 @@ class OneTFBS():
 
 #------------------------------ Notebook / script exceptions -----------------------------#
 
-#NOTE: not applied at the moment, but kept here for future uses
 def is_notebook():
 	""" Utility to check if function is being run from a notebook or a script """
 	try:
@@ -1077,24 +1076,28 @@ def _pvalues_for_chunks(TF_int_combinations, bg_dist_dict, alternative="greater"
 def _calculate_pvalue(mu, std, obs, alternative="greater"):
 	""" Calculate p-value of seeing value within the normal distribution with mu/std parameters"""
 	
-	z = (obs - mu)/std
-	p_oneside = scipy.stats.norm.sf(np.abs(z)) #one-sided pvalue
+	if std == 0: #not possible to calculate p-value using zscore
+		p = 0
 
-	#Calculate pvalue based on alternative hypothesis
-	if alternative == "two-sided":
-		p = p_oneside * 2
+	else:
+		z = (obs - mu)/std
+		p_oneside = scipy.stats.norm.sf(np.abs(z)) #one-sided pvalue
 
-	elif alternative == "greater":
-		if z > 0: #observed is larger than mean
-			p = p_oneside
-		else:
-			p = 1.0 - p_oneside
-	
-	elif alternative == "less":
-		if z < 0: #observed is smaller than mean
-			p = p_oneside
-		else:
-			p = 1.0 - p_oneside
+		#Calculate pvalue based on alternative hypothesis
+		if alternative == "two-sided":
+			p = p_oneside * 2
+
+		elif alternative == "greater":
+			if z > 0: #observed is larger than mean
+				p = p_oneside
+			else:
+				p = 1.0 - p_oneside
+		
+		elif alternative == "less":
+			if z < 0: #observed is smaller than mean
+				p = p_oneside
+			else:
+				p = 1.0 - p_oneside
 
 	return(p)
 
@@ -1105,21 +1108,31 @@ def remove_val_from_dist(value, mu, std, n):
 	Parameters
 	-----------
 	value : float
-		Value to remove. 
-		
+		The value to remove from distribution. 
+	mu : float
+		Mean of the distribution.
+	std : float
+		Standard deviation of the distribution.
 	n : int
-		The number of elements in the distribution (including value)
+		The number of elements in the distribution (including value).
 	
-	returns()
+	Returns
+	-----------
+		A tuple of (mean, std, n) for the new distribution
 	"""
+
+	zero_tol = 10**-10 #tolerance for zero due to decimal point rounding errors
 
 	#Calculate new mean
 	bg_mean = (mu*n - value)/(n-1)
 
 	#Calculate new std
-	var = std**2
-	bg_var = n/(n-1) * (var - (mu - value)**2/(n-1))
-	bg_std = np.sqrt(bg_var)
+	if bg_mean < zero_tol: #No other values left
+		bg_std = 0
+	else:
+		var = std**2
+		bg_var = (var*(n-1) - (value - bg_mean) * (value - mu))/(n-2)
+		bg_std = np.sqrt(bg_var)
 
 	bg_n = n - 1
 